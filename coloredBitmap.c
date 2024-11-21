6e2f15aba245f9af210b5b4713200941dcdedaa2
ab157157 <abdelaziz.oumnia@gmail.com>
2024-11-21 18:19:34 +0100

    update

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,29 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+  // Taille de base en octets
+   float sizeInBytes = w * h * 3; 
+
+   // Convertir en bits
+    if (strcmp(unit, "bt") == 0) {
+        return sizeInBytes * 8; 
+        
+   // Convertir en kilobytes  
+    } else if (strcmp(unit, "ko") == 0) {
+        return sizeInBytes / 1024; 
+
+   // Convertir en megabytes
+    } else if (strcmp(unit, "mo") == 0) {
+        return sizeInBytes / (1024 * 1024); 
+
+   // Convertir en gigabytes
+    } else if (strcmp(unit, "go") == 0) {
+        return sizeInBytes / (1024 * 1024 * 1024); 
+
+   // Unite inconnue
+    } else {
+        return 0; 
+    }
+}
 
    // YOUR CODE HERE - END
-   return 0;
-}
